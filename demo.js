cb60f1b09824951dd1cdf47d1f23314d93d19ae6
ALFI ALI <alfiali.333@gmail.com>
2024-05-02 10:02:27 -0700

    some change
    some change

--- demo.js
+++ demo.js
@@ -1,23 +1,34 @@
-function generateIcebreaker() {
-  const questions = [
-    "What's your favorite way to unwind after a long day of coding?",
-    "What hobby would you get into if time and money weren't an issue?",
-    "What's the last book you read or movie you watched that you really enjoyed?",
-    "If you could visit any country in the world, where would you go and why?",
-    "What's one skill you'd like to develop or hobby you'd like to take up?",
-    "What's the most memorable vacation you've ever had?",
-    "If you could have dinner with any historical figure, who would it be and why?",
-    "What's your favorite season of the year and why?",
-    "What’s your favorite dish to cook or eat?",
-    "If you could instantly become an expert in something, what would it be?",
+function generateName() {
+  const Name = [
+    "Hi:Alfi",
+    "Hi:Abdikadir",
+    "Hi:Andrea",
+    "Hi:Brett",
+    "Hi:Daniel",
+    "Hi:Dante",
+    "Hi:Lupe",
+    "Hi:Jaranesca",
+    "Hi:Jesus",
+    "Hi:Kaaliyah",
+    "Hi:Kadiatou",
+    "Hi:Kayla",
+    "Hi:Leslie",
+    "Hi:Maximo",
+    "Hi:Moncerrat",
+    "Hi:Niran",
+    "Hi:Remsey",
+    "Hi:Rene",
+    "Hi:Sabrina",
+    "Hi:Uriel",
+    "Hi:Victor",
   ];
 
-  // Randomly select a question
-  const index = Math.floor(Math.random() * questions.length);
+  // Randomly select a Name
+  const index = Math.floor(Math.random() * Name.length);
 
-  document.getElementById("displayQuestion").innerHTML = questions[index];
-  // return questions[index];
+  document.getElementById("displayName").innerHTML = Name[index];
+  // return Name[index];
 }
 
 // Example usage:
-let thequestion = generateIcebreaker();
+let thequestion = generateName();
